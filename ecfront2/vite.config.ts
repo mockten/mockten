7548805f7a347eb7920adf1eaa78ed5754fbf2c7
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-12-01 22:51:24 +0800

    Update vite.config.ts

--- ecfront2/vite.config.ts
+++ ecfront2/vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
-});
+  plugins: [react()],
+})
