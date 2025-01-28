6d04368430a7657be312b06021b5d2c4c7db92a8
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2025-01-28 23:13:57 +0800

    Update Taskfile and refactor API calls in CreateAccount and Login components

--- ecfront2/src/pages/CreateAccount.tsx
+++ ecfront2/src/pages/CreateAccount.tsx
@@ -28,8 +28,8 @@ const CreateAccount = () => {
   const [message, setMessage] = useState<string | null>(null);
   const [open, setOpen] = useState(false);
   const [severity, setSeverity] = useState<'success' | 'error'>('success');
-  const realm = 'mockten-realm-dev';
-  const keycloak = 'localhost:8080'
+  //const realm = 'mockten-realm-dev';
+  //const keycloak = 'localhost:8080'
 
   const validateEmail = (email: string) => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -75,7 +75,8 @@ const CreateAccount = () => {
     // params.append('password', 'seller');
 
     try {
-      const response = await fetch(`http://${keycloak}/realms/${realm}/protocol/openid-connect/token`, {
+      const response = await fetch(`/api/uam/token`, {     
+      //const response = await fetch(`http://${keycloak}/realms/${realm}/protocol/openid-connect/token`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -101,7 +102,7 @@ const CreateAccount = () => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // const apiUrl = process.env.REACT_APP_ADMIN_API;
-    const apiUrl = 'http://localhost:8080';
+    //const apiUrl = 'http://localhost:8080';
 
     const token = await getToken();
 
@@ -141,7 +142,8 @@ const CreateAccount = () => {
       };
 
       try {
-        const response = await fetch(`${apiUrl}/admin/realms/${realm}/users`, {
+        const response = await fetch(`/api/uam/users`, {
+        //const response = await fetch(`${apiUrl}/admin/realms/${realm}/users`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
